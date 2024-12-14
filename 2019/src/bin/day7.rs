b2bb75a9c96455a7d56ead014b86a353177b5a1e
datdenkikniet <jcdra1@gmail.com>
2024-12-14 18:31:02 +0100

    2019: day 7: part 2

--- 2019/src/bin/day7.rs
+++ 2019/src/bin/day7.rs
@@ -1,4 +1,4 @@
-use std::isize;
+use std::{isize, ops::Range};
 
 use aoc2019::ProgramState;
 
@@ -7,12 +7,13 @@ fn main() {
     let program: Vec<isize> = line.split(',').map(|v| v.parse().unwrap()).collect();
 
     part1(&program);
+    part2(&program);
 }
 
 fn part1(program: &[isize]) {
     let mut max_thruster_value = isize::MIN;
 
-    for settings in settings() {
+    for settings in settings(0..5) {
         let mut input = 0;
 
         for setting in settings {
@@ -23,12 +24,55 @@ fn part1(program: &[isize]) {
 
         max_thruster_value = max_thruster_value.max(input);
     }
-    println!("Thruster input: {max_thruster_value}");
+    println!("Part 1: {max_thruster_value}");
 }
 
-fn settings() -> impl Iterator<Item = [isize; 5]> {
-    fn rec(path: &mut Vec<isize>, out: &mut Vec<[isize; 5]>) {
-        for v in 0..5 {
+fn part2(program: &[isize]) {
+    let mut max_thruster_value = isize::MIN;
+
+    for setting in settings(5..10) {
+        let mut amplifiers = [
+            ProgramState::new_multi_input(vec![setting[0]], program.to_vec()),
+            ProgramState::new_multi_input(vec![setting[1]], program.to_vec()),
+            ProgramState::new_multi_input(vec![setting[2]], program.to_vec()),
+            ProgramState::new_multi_input(vec![setting[3]], program.to_vec()),
+            ProgramState::new_multi_input(vec![setting[4]], program.to_vec()),
+        ];
+
+        amplifiers.iter_mut().for_each(|a| {
+            assert!(a.poll().is_pending());
+        });
+
+        let mut input = 0;
+
+        loop {
+            let mut any_done = false;
+            for amplifier in amplifiers.iter_mut() {
+                assert_eq!(amplifier.input_len(), 0);
+
+                amplifier.add_input(input);
+
+                if amplifier.poll().is_ready() {
+                    any_done = true;
+                }
+
+                input = amplifier.output();
+            }
+
+            max_thruster_value = max_thruster_value.max(input);
+
+            if any_done {
+                break;
+            }
+        }
+    }
+
+    println!("Part 2: {max_thruster_value}");
+}
+
+fn settings(range: Range<isize>) -> impl Iterator<Item = [isize; 5]> {
+    fn rec(range: Range<isize>, path: &mut Vec<isize>, out: &mut Vec<[isize; 5]>) {
+        for v in range.clone() {
             if path.contains(&v) {
                 continue;
             }
@@ -46,7 +90,7 @@ fn settings() -> impl Iterator<Item = [isize; 5]> {
                 out.push(value);
             }
 
-            rec(path, out);
+            rec(range.clone(), path, out);
 
             path.pop();
         }
@@ -54,7 +98,7 @@ fn settings() -> impl Iterator<Item = [isize; 5]> {
 
     let mut out = Vec::new();
     let mut path = Vec::new();
-    rec(&mut path, &mut out);
+    rec(range, &mut path, &mut out);
 
     out.into_iter()
 }
