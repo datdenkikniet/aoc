0165ad3bbdc3db2761e261646d6f4f4414cf0283
datdenkikniet <jcdra1@gmail.com>
2024-12-19 08:10:08 +0100

    2024: day 19: part 2

--- 2024/src/bin/day19.rs
+++ 2024/src/bin/day19.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     let mut lines = std::io::stdin().lines().map(|v| v.unwrap());
 
@@ -11,6 +13,7 @@ fn main() {
     let desired_patterns: Vec<_> = lines.skip(1).collect();
 
     part1(&available_towels, &desired_patterns);
+    part2(&available_towels, &desired_patterns);
 }
 
 fn part1(towels: &[String], desired_patterns: &[String]) {
@@ -37,4 +40,41 @@ fn is_possible(towels: &[String], pattern: &str) -> bool {
     }
 
     false
+}
+
+fn part2(towels: &[String], desired_patterns: &[String]) {
+    let mut sum = 0;
+
+    let mut known = HashMap::new();
+    for pattern in desired_patterns {
+        sum += possible_patterns(towels, pattern, &mut known);
+    }
+
+    println!("Part 2: {sum}");
+}
+
+fn possible_patterns(
+    towels: &[String],
+    pattern: &str,
+    known: &mut HashMap<String, usize>,
+) -> usize {
+    if pattern.is_empty() {
+        return 1;
+    }
+
+    if let Some(known) = known.get(pattern).cloned() {
+        return known;
+    }
+
+    let mut patterns = 0;
+
+    for towel in towels {
+        if pattern.starts_with(towel) {
+            patterns += possible_patterns(towels, &pattern[towel.len()..], known);
+        }
+    }
+
+    known.insert(pattern.to_string(), patterns);
+
+    patterns
 }
