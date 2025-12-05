ec72cc195dba0ae2484b9b45d1b538083dc9aa59
datdenkikniet <jcdra1@gmail.com>
2025-12-05 21:16:00 +0100

    2025 day 5 part 2

--- 2025/src/bin/day5.rs
+++ 2025/src/bin/day5.rs
@@ -20,6 +20,7 @@ fn main() {
     let ingredients: Vec<usize> = lines.map(|v| v.parse().unwrap()).collect();
 
     part1(&fresh_ranges, &ingredients);
+    part2(&fresh_ranges);
 }
 
 fn part1(fresh_ranges: &[RangeInclusive<usize>], ingredients: &[usize]) {
@@ -32,5 +33,33 @@ fn part1(fresh_ranges: &[RangeInclusive<usize>], ingredients: &[usize]) {
             }
         }
     }
+
     println!("Part 1: {fresh}");
+}
+
+fn part2(fresh_ranges: &[RangeInclusive<usize>]) {
+    let mut ranges: Vec<_> = fresh_ranges.iter().cloned().collect();
+    ranges.sort_unstable_by_key(|v| *v.start());
+
+    let mut new_ranges = vec![ranges[0].clone(); 1];
+
+    for range in ranges.into_iter().skip(1) {
+        let prev = new_ranges.last().unwrap();
+
+        if prev.contains(range.start()) && prev.contains(range.end()) {
+            continue;
+        }
+
+        let range = if range.start() > prev.end() {
+            range.clone()
+        } else {
+            prev.end() + 1..=*range.end()
+        };
+
+        new_ranges.push(range);
+    }
+
+    let sum: usize = new_ranges.into_iter().map(|v| v.count()).sum();
+
+    println!("Part 2: {}", sum);
 }
